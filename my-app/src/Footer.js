099607b2fc9a3b0f2899b4be4f1ef5b80a57bace
Kiprotich <fleeklawi@gmail.com>
2022-05-21 20:05:16 +0300

    Added content - footer.js, header.js

---
+++ my-app/src/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Footer() {
+    <div>
+        <div className='footer--col'>
+            <ul>
+                <li><a href=''></a></li>
+                <li><a href=''></a></li>
+                <li><a href=''></a></li>
+            </ul>
+        </div>
+        <div className='footer--col'>
+            <ul>
+                <li>Developers</li>
+                <li>TERMS |</li>
+                <li>CONDITIONS |</li>
+                <li>PRIVACY</li>
+            </ul>
+        </div>
+        <div className='footer--col'>
+            <span>SEND US A MESSAGE: <input type={text} /><button>Send</button></span>
+        </div>
+    </div>
+}
---
+++ my-app/src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Header() {
+    <div>
+        <div>GUIDE INTO THE DARKWEB - UI</div>
+        <nav>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Join Us</a></li>
+            </ul>
+        </nav>
+
+        <div className='header--content'>
+            <h3>Get to know the Why, Where, How, Pro's and Con's of the Darkweb!</h3>
+            <button>Learn More</button>
+            <h6>scroll down</h6>
+        </div>
+    </div>
+}
